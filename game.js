680361010e0dab0f6bedbeb7073d4cf07892a237
Hodaya Tzadok <hodayatzadok22@gmail.com>
2022-05-23 22:47:10 +0300

    change

--- game.js
+++ game.js
@@ -1,80 +1,85 @@
-const cards=['🍉','🍹','☀️','🌊','🤿','🍉','🍹','☀️','🌊','🤿']
-let score=0;
-let s=document.querySelector("#score")
-const cardBack="o";
-let arrayId=[]
-const board =document.getElementById("board");
-  // Player object
-  function Player(id, name) {
-    this.id = id;
-    this.name = name;
-    this.cards = [];
-    this.active = false;
-    this.addScore = function () {
-      this.pairs++;
-    };
-    this.showMe = function () {
-      document.getElementById(
-        `player${id}`
-      ).innerHTML = `<div name="player" id="player1">${this.name} found ${this.pairs} pairs</div>`;
-    };
-    this.myTurn = function () {
-      document.getElementById(`player${id}`).classList.add("activePlayer");
-    };
+const cards = ["🍉", "🍹", "☀️", "🌊", "🤿", "🍉", "🍹", "☀️", "🌊", "🤿"];
+let score1 = 0;
+let score2 = 0;
+
+const cardBack = "o";
+let arrayId = [];
+const board = document.getElementById("board");
+const win = document.getElementById("win");
+
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+const $score_player1 = document.getElementById("score_player1");
+const $score_player2 = document.getElementById("score_player2");
+let toggle = true;
+let player_class = "player1_class";
+
+// function changeClassName(className) {
+//   console.log("in func");
+//   // player1.classList.add("player1_class");
+//   if (className == "player1") {
+//     player1.classList.(`player1_class`);
+//   } else {
+//     player2.classList.replace(`player2_class`);
+//   }
+// }
+// changeClassName("player1");
+
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[random]] = [arr[random], arr[i]];
   }
-  const numOfPlayers = 2;
-  if (
-    document.getElementById(`player1`).value == undefined ||
-    document.getElementById(`player2`).value == undefined
-  ) {
-    for (let p = 0; p < numOfPlayers; p++) {
-      let playerName = document.getElementById(`player${p + 1}`).value;
-      players.push(new Player(p + 1, playerName));
-    }
-    activePlayer = players[0];
-    players[0].active = true;
-    players.forEach((v) => v.showMe());
-    players[0].myTurn();
-  }
-function shuffle(arr){
-    for(let i =arr.length-1;i>0;i--){
-        const random=Math.floor(Math.random()*(i+1));
-      [arr[i],arr[random]]=[arr[random],arr[i]];
-    }
-    return arr;
+  return arr;
 }
 
-function createCard(card){
-    const cardEl = document.createElement("div")
-    cardEl.innerHTML=cardBack
-    cardEl.id = i;
-    cardEl.className="card";
-    return cardEl;
-  }
-  shuffle(cards);
-  for(i in cards){ 
-    const cardBack1=createCard(i);
-    board.appendChild(cardBack1);
-  }
-  
-  board.addEventListener('click',(e) =>{
-    if(e.target == board){}
-    else{
-    e.target.innerHTML = cards[e.target.id]
-    const cardId=e.target
-    arrayId.push(cardId)
-    if(arrayId.length==2){
-      if(arrayId[0].innerHTML == arrayId[1].innerHTML){
-        score++;
-        s.textContent=`score: ${score}`;
-        arrayId= [];
-      }else{
-        setTimeout(()=>{
-        arrayId[0].innerHTML = cardBack;
-        arrayId[1].innerHTML = cardBack;
-        arrayId= [];
-      },300)
-    }
-  }}
-  })
+function createCard(card) {
+  const cardEl = document.createElement("div");
+  cardEl.innerHTML = cardBack;
+  cardEl.id = i;
+  cardEl.className = "card";
+  return cardEl;
+}
+shuffle(cards);
+for (i in cards) {
+  const cardBack1 = createCard(i);
+  board.appendChild(cardBack1);
+}
 
+board.addEventListener("click", (e) => {
+  if (e.target == board) {
+  } else {
+    e.target.innerHTML = cards[e.target.id];
+    const cardId = e.target;
+    arrayId.push(cardId);
+    if (arrayId.length == 2) {
+      if (arrayId[0].innerHTML == arrayId[1].innerHTML) {
+        if (toggle) {
+          score1++;
+          $score_player1.textContent = `score: ${score1}`;
+
+          // changeClassName("player1");
+        } else {
+          score2++;
+          $score_player2.textContent = `score: ${score2}`;
+          // changeClassName("player2");
+        }
+        toggle = !toggle;
+        arrayId = [];
+      } else {
+        setTimeout(() => {
+          arrayId[0].innerHTML = cardBack;
+          arrayId[1].innerHTML = cardBack;
+          arrayId = [];
+        }, 300);
+      }
+    }
+  }
+  if (score1 + score2 == cards.length / 2) {
+    if (score1 > score2) {
+      win.textContent = "player 1 you win";
+    } else {
+      win.textContent = "player 2 you win";
+    }
+  }
+});
